2b036789537e9421dbd5b934ff1569ee4deb0f96
mfo <fourcade.m@gmail.com>
2014-07-08 17:04:13 +0200

    feat(media): add handle assets_parameters

--- lib/dm_cloud/builder/media.rb
+++ lib/dm_cloud/builder/media.rb
@@ -1,22 +1,38 @@
 module DmCloud
   module Builder
     module Media
-      def self.create(url = '', assets_names = [], meta = {})
+      # This method can either create an empty media object
+      # or also download a media with the url paramater
+      # and use it as the source to encode the ASSET_NAME listed in assets_names
+      # Params :
+      #   args:
+      #     url: SCHEME://USER:PASSWORD@HOSTNAME/MY/PATH/FILENAME.EXTENSION (could be ftp or http)
+      #     author: an author name
+      #     title: a title for the film
+      #     assets_names: (Array) – (optional) the list of ASSET_NAME you want to transcode,
+      #       when you set this parameter you must also set the url parameter
+      # Return :
+      #   media_id: return the media id of the object
+      def self.create(options = {})
         request = Hash.new
+        url = options['url']
+        assets_names = options['assets_names']
+        assets_parameters = options['assets_parameters']
+        meta = options['meta']
 
         request['url'] = url
 
-        if not meta.empty?
+        if !meta.empty?
           request['meta'] = {}
-          request['meta']['author'] = meta[:author] if meta[:author]
-          request['meta']['author'] = meta[:title] if meta[:title]
+          request['meta']['author'] = meta['author'] if meta['author']
+          request['meta']['author'] = meta['title'] if meta['title']
         end
 
-        request['assets_names'] = assets_names if not assets_names.empty?
-
+        request['assets_names'] = assets_names unless assets_names.empty?
+        request['assets_parameters'] = assets_parameters unless assets_parameters.empty?
         request.rehash
       end
-      
+
       def self.info(media_id, assets_names = ['source'], fields = {})
         raise StandardError, "missing :media_id in params" unless media_id
         request = Hash.new
@@ -47,7 +63,7 @@ module DmCloud
         end
         request
       end
-      
+
       def self.list(page = 1, per_page = 10, fields = {})
         # raise StandardError, "missing :media_id in params" unless media_id
         request = Hash.new
@@ -68,7 +84,7 @@ module DmCloud
         # fields[:stats].each { |key| request << "meta.#{key.to_s}" } if fields[:meta].present?
         # request['stats'][COUNTRY_CODE][TIME_INTERVAL] : the statistics on the number of views in a specific country (eg: stats.fr.total, stats.us.last_week, etc...)
         # request['extended_stats'][COUNTRY_CODE][TIME_INTERVAL]
-        
+
         assets_names = ['source'] if assets_names.nil?
         if not fields[:assets]
           request = all_assets_fields(request, assets_names)
